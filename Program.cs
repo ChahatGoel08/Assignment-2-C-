b0ce4024a47f0e7af6115bef3a3de34f55ae4984
CHAHAT GOEL <86407728+ChahatGoel08@users.noreply.github.com>
2022-01-16 20:12:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace My_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("I failed the first quarter of a class in middle school,..");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("...I did this every quarter that year. \nI forgot that they mail home the end-of-year cards, and my mom got it before ...");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("She was...");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("—at the school for their error... \nThe teacher also retired that year and had already thrown out his records,");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("(the fake ones I made throughout the year) and .....");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.ReadLine();
+            Console.ReadKey();
+            
+        }
+    }
+}
